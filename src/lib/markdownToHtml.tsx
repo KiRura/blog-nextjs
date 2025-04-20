b1de47c214c3b69652dffe1cfb656bb028ebb0eb
KiRura <57588049+KiRura@users.noreply.github.com>
2025-04-20 19:52:57 +0900

    なんだこれ

--- src/lib/markdownToHtml.tsx
+++
@@ -1,6 +0,0 @@
-import Markdown from "react-markdown";
-import remarkGfm from "remark-gfm";
-
-export default function markdownToHtml(markdown: string) {
-	return <Markdown remarkPlugins={[remarkGfm]}>{markdown}</Markdown>;
-}
